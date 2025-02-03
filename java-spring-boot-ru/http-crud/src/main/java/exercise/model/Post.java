4d51cd627e483f4de4add35176cbe54f7fde681c
@hexlet/cli <support@hexlet.io>
2025-02-03 15:50:04 +0300

    submit java-spring-boot-ru/http-crud

---
+++ java-spring-boot-ru/http-crud/src/main/java/exercise/model/Post.java
@@ -0,0 +1,17 @@
+package exercise.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Post {
+    private String id;
+    private String title;
+    private String body;
+
+}
